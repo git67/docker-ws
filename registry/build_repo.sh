fdbb461dad6b89cd7fe979fd4f9b2f8901186363
k8s-com <k8s-community@gmx.de>
2022-10-04 11:50:12 +0200

    registry

--- registry/build_repo.sh
+++ registry/build_repo.sh
@@ -8,6 +8,12 @@ CRT="${HOST}.crt"
 KEY="${HOST}.key"
 MAPPED_PORT="5000"
 
+ID=$(docker ps | grep registry | awk '{print $1}')
+if [ "x${ID}" != "x" ]; then
+	docker stop ${ID}
+	docker rm ${ID}
+fi
+
 if [ ! -d ${LOCAL_CERTS} ]; then
         mkdir -p ${LOCAL_CERTS}
 fi
@@ -22,8 +28,9 @@ openssl x509 -in ${LOCAL_CERTS}/${CRT} -noout -text
 if [ ! -d /etc/docker/certs.d/${HOST}:${MAPPED_PORT} ]; then
         mkdir -p /etc/docker/certs.d/${HOST}:${MAPPED_PORT}
 fi
-        cp ${LOCAL_CERTS}/${CRT} /etc/docker/certs.d/${HOST}:${MAPPED_PORT}/ca.crt
-        chmod 0400 /etc/docker/certs.d/${HOST}:${MAPPED_PORT}/ca.crt
+cp ${LOCAL_CERTS}/${CRT} /etc/docker/certs.d/${HOST}:${MAPPED_PORT}/ca.crt
+chmod 0644 /etc/docker/certs.d/${HOST}:${MAPPED_PORT}/ca.crt
+chown ${USER}: /etc/docker/certs.d/${HOST}:${MAPPED_PORT}/ca.crt
 
 docker run -d --restart=always --name registry -v ${LOCAL_CERTS}:/certs -v ${LOCAL_STORE}:/var/lib/registry \
  -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/${CRT} \
---
+++ registry/cp_ca.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+USER="hs"
+HOST="dn"
+REG_HOST="d-reg"
+MAPPED_PORT="5000"
+
+
+
+ssh ${USER}@${HOST} "sudo mkdir -p /etc/docker/certs.d/${REG_HOST}:${MAPPED_PORT}" 
+
+scp /etc/docker/certs.d/${REG_HOST}:${MAPPED_PORT}/ca.crt ${USER}@${HOST}:~/
+
+#ssh ${USER}@${HOST} "sudo chown ${USER}: ${USER}@${HOST}:~/ca.crt"
+
+ssh ${USER}@${HOST} "sudo cp ~/ca.crt /etc/docker/certs.d/${REG_HOST}:${MAPPED_PORT}/"
+ssh ${USER}@${HOST} "sudo chown ${USER}: /etc/docker/certs.d/${REG_HOST}:${MAPPED_PORT}/*"
