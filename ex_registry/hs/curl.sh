36e7a37a563c4a6930f87c727285cef04e8dcf09
Heiko Stein <hs@heiko-stein.de>
2022-10-11 09:04:56 +0200

    add do user

---
+++ ex_registry/hs/curl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#docker pull nginx
+#docker images
+#docker tag nginx d-reg:5000/develop/nginx
+#docker push d-reg:5000/develop/nginx
+
+
+curl --cacert /etc/docker/certs.d/d-reg\:5000/ca.crt -X GET https://d-reg:5000/v2/_catalog
+
+curl --cacert /etc/docker/certs.d/d-reg\:5000/ca.crt -X GET https://d-reg:5000/v2/develop/nginx/tags/list
+
+curl --cacert /etc/docker/certs.d/d-reg\:5000/ca.crt -X GET https://d-reg:5000/v2/develop/nginx/manifests/latest
+
+curl --head -H application/vnd.docker.distribution.manifest.v2+json --cacert /etc/docker/certs.d/d-reg\:5000/ca.crt -X GET https://d-reg:5000/v2/develop/nginx/manifests/latest 
+
+# curl --cacert /etc/docker/certs.d/d-reg\:5000/ca.crt -X DELETE https://d-reg:5000/v2/httpd/manifests/sha256:1dc61f5a5f3ddc2a81be7eb3b2ba5dfbac49f49f69d429ea4f3ae77a7ea2f768
+
+
